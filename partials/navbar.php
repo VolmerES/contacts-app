836d4ff6f1642995f21135494d77c7fa785edc20
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2026-05-13 18:50:57 +0200

    Update navbar links and add user registration functionality

--- partials/navbar.php
+++ partials/navbar.php
@@ -18,10 +18,10 @@
 	<div class="collapse navbar-collapse" id="navbarNav">
 	<ul class="navbar-nav">
 		<li class="nav-item">
-		<a class="nav-link" href="home.php">Home</a>
+		<a class="nav-link" href="register.php">Register</a>
 		</li>
 		<li class="nav-item">
-		<a class="nav-link" href="add.php">Add Contact</a>
+		<a class="nav-link" href="login.php">Login</a>
 		</li>
 	</ul>
 	</div>
