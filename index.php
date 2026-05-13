c30347fee30c1438197605a825402b775cffc79e
Volmer42 <77463801+VolmerES@users.noreply.github.com>
2026-05-13 17:28:18 +0200

    Refactor contact management: separate header, footer, and navbar into partials; update form validation for phone number

--- index.php
+++ index.php
@@ -5,65 +5,8 @@ $contacts = $conn->query("SELECT * FROM contacts");
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php require "partials/header.php" ?>
 
-  <!-- Bootstrap -->
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css"
-    integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
-  />
-  <script
-    defer
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"
-  ></script>
-
-  <!-- Static Content -->
-  <link rel="stylesheet" href="./static/css/index.css" />
-
-  <title>Contacts App</title>
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand font-weight-bold" href="#">
-        <img class="mr-2" src="./static/img/logo.png" />
-        ContactsApp
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./add.php">Add Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <main>
     <div class="container pt-4 p-3">
       <div class="row">
 
@@ -83,7 +26,7 @@ $contacts = $conn->query("SELECT * FROM contacts");
         	      <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
         	      <p class="m-2"><?= $contact["phone_number"] ?>
 				  </p>
-        	      <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
+        	      <a href="edit.php?id=<?= $contact["id"] ?>" class="btn btn-secondary mb-2">Edit Contact</a>
         	      <a href="delete.php?id=<?= $contact["id"] ?>" class="btn btn-danger mb-2">Delete Contact</a>
         	    </div>
         	  </div>
@@ -92,6 +35,6 @@ $contacts = $conn->query("SELECT * FROM contacts");
 
       </div>
     </div>
-  </main>
-</body>
-</html>
+
+
+<?php require "partials/footer.php" ?>
